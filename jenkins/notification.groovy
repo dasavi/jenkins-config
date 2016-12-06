f8accee939008fd80012348ffc6429fafb66d031
Avishek Das <suavidas@gmail.com>
2016-12-06 12:36:10 -0600

    test load

--- jenkins/notification.groovy
+++ jenkins/notification.groovy
@@ -1,21 +1,21 @@
-public class Config {
-    static List<String> emailRecipients = []
-    static List<String> slackChannels = []
-    static String slackTeam = ''
-    static String slackToken = ''
-}
+//public class Config {
+//    static List<String> emailRecipients = []
+//    static List<String> slackChannels = []
+//    static String slackTeam = ''
+//    static String slackToken = ''
+//}
 
 public class ColorCode {
     static String SUCCESS = '#00FF00'
     static String FAIL = '#FF0000'
 }
 
-def notifyBuild(Config config, boolean buildSuccess) {
+def notifyBuild(List<String> emailRecipients, String slackTeam, String slackToken, List<String> slackChannels, boolean buildSuccess) {
     setBuildColor(buildSuccess)
     // Send notifications
     String buildMessage = buildSuccess ? "SUCCESS" : "FAIL"
-    sendSlack(getNotificationColor(buildSuccess), buildMessage)
-    sendEmail(buildMessage)
+    sendSlack(slackTeam, slackToken, slackChannels, getNotificationColor(buildSuccess), buildMessage)
+    sendEmail(emailRecipients, buildMessage)
 }
 
 def setBuildColor(boolean buildSuccess) {
@@ -26,17 +26,17 @@ def getNotificationColor(boolean buildSuccess) {
     return buildSuccess ? ColorCode.SUCCESS : ColorCode.FAIL
 }
 
-def sendSlack(Config config, String colorCode, String statusString) {
+def sendSlack(String slackTeam, String slackToken, List<String> slackChannels, String colorCode, String statusString) {
     def summary = "${buildNotificationSubject(statusString)} (${env.BUILD_URL})"
 
-    for(channel in config.slackChannels) {
-        slackSend channel: channel, color: colorCode, message: summary, teamDomain: config.slackTeam, token: config.slackToken
+    for(channel in slackChannels) {
+        slackSend channel: channel, color: colorCode, message: summary, teamDomain: slackTeam, token: slackToken
     }
 }
 
-def sendEmail(Config config, String statusString) {
-    if(config.emailRecipients.size() > 0) {
-        String recipients = config.emailRecipients.join(",");
+def sendEmail(List<String> emailRecipients, String statusString) {
+    if(emailRecipients.size() > 0) {
+        String recipients = emailRecipients.join(",");
         emailext (
                 subject: buildNotificationSubject(statusString),
                 body: buildEmailBody(statusString),
@@ -52,6 +52,10 @@ def buildEmailBody(String statusString) {
 
 def buildNotificationSubject(String statusString) {
     return "${statusString}: ${env.JOB_NAME} [${env.BUILD_NUMBER}]"
+}
+
+def testLoad() {
+    System.out.println("Test Load success")
 }
 
 return this
